522282ccf03e560b3a8840b81651e7c795cf1124
MSI <MSI@DESKTOP-K3TGPV6>
2022-01-23 22:49:17 +0300

    PhotoStock.API impl

---
+++ Services/PhotoStock/FreeCourse.Service.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,45 @@
+﻿using FreeCource.Shared.ControllerBases;
+using FreeCource.Shared.Dtos;
+using FreeCourse.Service.PhotoStock.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Service.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomControllerBase
+    {
+        [HttpPost]
+        public  async Task<IActionResult> PhotoSave(IFormFile photo,CancellationToken cancellationToken) // cancellationtoken => kişi dosyayı kaydederken browseri kapatırsa dosya yükleme işini otomatik olarak sonlandır. 
+        {
+            if(photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+                var returnPath = $"photos/{photo.FileName}";
+
+                PhotoDto photoDto = new() { Url = returnPath };
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 404));
+        }
+
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
